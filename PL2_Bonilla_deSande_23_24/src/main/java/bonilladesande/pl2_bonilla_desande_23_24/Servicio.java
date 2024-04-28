da398a26c94d30af8be9b2b11a663ddd2822f532
Botiquim <150058792+Botiquim@users.noreply.github.com>
2024-04-28 20:31:00 +0200

    Cambios en el servicio

--- PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/Servicio.java
+++ PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/Servicio.java
@@ -10,6 +10,28 @@ package bonilladesande.pl2_bonilla_desande_23_24;
  */
 import java.util.ArrayList;
 class Servicio {
+
+   
     public String nombreServicio;
+    
+    public Servicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+        
+    }
+    
+     @Override
+    public String toString() {
+        return "Servicio{" + "nombreServicio=" + nombreServicio + '}';
+    }
+
+    
     
 }
