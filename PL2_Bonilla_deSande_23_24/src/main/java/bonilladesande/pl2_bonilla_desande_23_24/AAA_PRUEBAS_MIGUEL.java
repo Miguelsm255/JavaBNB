8ccc7f0723bab5bdbbfefad4f461803fb28f2c24
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-05-15 13:41:06 +0200

    login no funciona
    Funciona cuando los datos son introducidos directamente en el código, pero no cuando los introduce el usuario (ya sea por terminal o por GUI) y creo que es por el formato que le da java al introducir los datos. Hay que investigar.

--- PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/AAA_PRUEBAS_MIGUEL.java
+++ PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/AAA_PRUEBAS_MIGUEL.java
@@ -1,13 +1,15 @@
 package bonilladesande.pl2_bonilla_desande_23_24;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class AAA_PRUEBAS_MIGUEL {
 
  
     public static void main(String[] args) {
                 
-        Cliente c1 = new Cliente("12345678w","Miguel","aa","aa",669928305);
+        Scanner scanner = new Scanner(System.in);
+        Cliente c1 = new Cliente("12345678w","Miguel","hola","hola",669928305);
     
         LocalDate ahora = LocalDate.now();
         Anfitrion a1 = new Anfitrion(ahora, "78945612G", "Marcos", "hola", "fdsafdsa", 789654123);
@@ -17,10 +19,22 @@ public class AAA_PRUEBAS_MIGUEL {
         
         //CrearCuenta.crearCuenta(false, "11", "pepe", "adios", "fdsafdsa", 0, ahora);
         
-        bonilladesande.pl2_bonilla_desande_23_24.GUI.Login ventanaLogin = new bonilladesande.pl2_bonilla_desande_23_24.GUI.Login();
-        ventanaLogin.setVisible(true);
+        BaseDatos.clientes.add(c1);
+        System.out.println(BaseDatos.clientes.get(0).getCorreo());
+        System.out.println(BaseDatos.clientes.get(0).getClave());
+        
+        //bonilladesande.pl2_bonilla_desande_23_24.GUI.Login ventanaLogin = new bonilladesande.pl2_bonilla_desande_23_24.GUI.Login();
+        //ventanaLogin.setVisible(true);
+        
+        String correo = scanner.nextLine();
+        String clave = scanner.nextLine();
+        
+        char[] correoChar = correo.toCharArray();
+        
+        System.out.println(correoChar.length);
         
         
+        Login.login(correo, clave );
         
         //UserLoged resultado = Login.login("miguelsm255@outlook.es","holacaracola");
     }
