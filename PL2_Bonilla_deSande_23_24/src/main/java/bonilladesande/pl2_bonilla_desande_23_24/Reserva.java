0595c10f41e21090b241ed1fedb15aff7d2c8b7b
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-05-21 19:46:44 +0200

    Clases factura y reservas
    creados los atributos y algunos métodos para crear la factura y la reserva y gestionarlas. Getters y setters y constructores

--- PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/Reserva.java
+++ PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/Reserva.java
@@ -1,5 +1,96 @@
 package bonilladesande.pl2_bonilla_desande_23_24;
 
+import java.time.LocalDate;
+
 public class Reserva {
+    
+    // ATRIBUTOS
+    
+    private Factura factura;
+    private Particular particular;
+    private Inmueble inmueble;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private double precio;
+    
+    
+    // MÉTODOS
+    
+    public static double pago(double precioNoche, int noches, boolean vip){
+        return -1;
+    }
+    
+    public static void cancelarReserva(int posReserva){
+        BaseDatos.reservas.remove(posReserva);
+    }
+    
+    public Factura generarFactura(Particular particular, Inmueble inmueble, LocalDate fechaInicio, LocalDate fechaFin, double precio){
+        Factura factura = new Factura(particular, inmueble, fechaInicio, fechaFin, precio);
+        return factura;
+    }
+
+    
+    
+    
+    // GETTERS Y SETTERS
+    
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public Particular getParticular() {
+        return particular;
+    }
+
+    public void setParticular(Particular particular) {
+        this.particular = particular;
+    }
+
+    public Inmueble getInmueble() {
+        return inmueble;
+    }
+
+    public void setInmueble(Inmueble inmueble) {
+        this.inmueble = inmueble;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+    // CONSTRUCTOR
+
+    public Reserva(Particular particular, Inmueble inmueble, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.particular = particular;
+        this.inmueble = inmueble;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+    
     
 }
