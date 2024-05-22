3bc7e5280c6cb6e4be39f9a8017dcd42dcf7404e
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-05-22 18:21:55 +0200

    Cambios menores importantes
    Integración de datos del usuario en la página de AjustesUsuario;
    Botón para ir a los usuarios formateado y se oculta cuando está en la página de usuario;

--- PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/GUI/ObjetoGestorVentanas.java
+++ PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/GUI/ObjetoGestorVentanas.java
@@ -1,5 +1,7 @@
 package bonilladesande.pl2_bonilla_desande_23_24.GUI;
 
+import bonilladesande.pl2_bonilla_desande_23_24.BaseDatos;
+
 public class ObjetoGestorVentanas {
     
     Login ventanaLogin;
@@ -57,12 +59,24 @@ public class ObjetoGestorVentanas {
                 paginaPaginaPrincipal = new PaginaPrincipal();
                 ventanaJavaBNB.frameJavaBNB.setViewportView(paginaPaginaPrincipal);
                 ventanaJavaBNB.setVisible(true);
+                ventanaJavaBNB.usuarioJavaBNBboton.setVisible(true);
+                ventanaJavaBNB.fotoUsuarioJavaBNBboton.setVisible(true);
+                
+                ventanaJavaBNB.usuarioJavaBNBboton.setText(BaseDatos.user.getNombre());
             }
             
             case "AjustesUsuario" -> {
                 paginaAjustesUsuario = new AjustesUsuario();
                 ventanaJavaBNB.frameJavaBNB.setViewportView(paginaAjustesUsuario);
+                ventanaJavaBNB.usuarioJavaBNBboton.setVisible(false);
+                ventanaJavaBNB.fotoUsuarioJavaBNBboton.setVisible(false);
                 ventanaJavaBNB.setVisible(true);
+                
+                paginaAjustesUsuario.saludoLabelAjustesUsuario.setText("Hola, " + BaseDatos.user.getNombre() + "!");
+                paginaAjustesUsuario.nombreLabelAjustesUsuario.setText(BaseDatos.user.getNombre());
+                paginaAjustesUsuario.claveLabelAjustesUsuario.setText(BaseDatos.user.getClave());
+                paginaAjustesUsuario.correoLabelAjustesUsuario.setText(BaseDatos.user.getCorreo());
+                paginaAjustesUsuario.telefonoLabelAjustesUsuario.setText(String.valueOf(BaseDatos.user.getTelefono()));
             }
         }
         
@@ -77,12 +91,12 @@ public class ObjetoGestorVentanas {
             
             case "PaginaPrincipal" -> {
                 ventanaJavaBNB.frameJavaBNB.remove(paginaPaginaPrincipal);
-                
+                 
             }
             
             case "AjustesUsuario" -> {
                 ventanaJavaBNB.frameJavaBNB.remove(paginaAjustesUsuario);
-                
+                    
             }
         }
         
