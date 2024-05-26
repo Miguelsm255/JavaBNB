c3fe2432bd921ed98f2fd0dd758902aa9ec5c2de
Botiquim <150058792+Botiquim@users.noreply.github.com>
2024-05-26 18:17:53 +0200

    Ya hemos estado mirando lo de la reserva, hemos creado la interfaz
    y solo quedaría ajustarlo con la nueva interfaz de subir alojamientos y poner la función de que un inmueble esté reservado durante un periodo específico de tiempo

--- PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/BibliotecaExcepciones.java
+++ PL2_Bonilla_deSande_23_24/src/main/java/bonilladesande/pl2_bonilla_desande_23_24/BibliotecaExcepciones.java
@@ -9,60 +9,75 @@ package bonilladesande.pl2_bonilla_desande_23_24;
  * @author marco
  */
 public class BibliotecaExcepciones {
-    
+
     public static class TelefonoEscacharrado extends Exception {
 
         public TelefonoEscacharrado(String message) {
             super(message);
         }
     }
-    
+
     public static class CorreoRepetido extends Exception {
 
         public CorreoRepetido(String message) {
             super(message);
         }
     }
+
+    public static class FechaMenor extends Exception {
+
+        public FechaMenor(String message) {
+            super(message);
+        }
+    }
     
+    public static class FechaInicioMenorFin extends Exception {
+
+        public FechaInicioMenorFin(String message) {
+            super(message);
+        }
+    }
+    
+    public static class InmuebleYaReservado extends Exception {
+
+        public InmuebleYaReservado(String message) {
+            super(message);
+        }
+    }
+
     public static class cvvEscacharrado extends Exception {
 
         public cvvEscacharrado(String message) {
             super(message);
         }
     }
-    
+
     public static class TarjetaEscacharrada extends Exception {
 
         public TarjetaEscacharrada(String message) {
             super(message);
         }
     }
+
     public static class CamposVacios extends Exception {
 
         public CamposVacios(String message) {
             super(message);
         }
     }
-    
+
     public static class BotonesSinSeleccionar extends Exception {
 
         public BotonesSinSeleccionar(String message) {
             super(message);
         }
     }
-    
+
     public static class TarjetaCaducada extends Exception {
 
         public TarjetaCaducada(String message) {
             super(message);
         }
     }
-    
 
 }
-
-
-
-
-
-
